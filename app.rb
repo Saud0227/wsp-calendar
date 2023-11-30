909e38c1a35128212452c6d71744319b5f7535e2
Saud0227 <Saud02Tech@gmail.com>
2023-11-30 14:26:03 +0100

    lesson work

--- app.rb
+++ app.rb
@@ -7,6 +7,21 @@ class Calendar < Sinatra::Base
         return @db
     end
 
+    get '/' do
+        @doors = db.execute('SELECT * FROM doors')
+
+        erb :index
+    end
+
+
+    get '/doors/:day' do
+        @door = db.execute('SELECT * FROM doors WHERE day = ?', params[:day]).first
+        puts @door
+        db.execute('UPDATE doors SET opened = true WHERE day = ?', params[:day])
+        puts @door
+        erb :'door/show'
+    end
+
 end
 
 
